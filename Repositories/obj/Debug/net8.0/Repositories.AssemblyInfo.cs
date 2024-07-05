58f91b2db9ee686c2ef47e5576c94e55d045cb77
NET_05_TruongHaiMinh <minhthse171011@fpt.edu.vn>
2024-07-05 14:18:14 +0700

    fix background register

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fce1ddcd285d6b3d7c677c28fc3dec77bd65ffd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6ae01f91fd679242bd820471ebcfcba7938325")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
